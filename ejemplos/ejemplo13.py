1759e16e678d7ee163a7f3444649c43d8d5d2788
rirastorza <50217531+rirastorza@users.noreply.github.com>
2020-08-10 15:16:24 -0300

    Add files via upload

--- ejemplos/ejemplo13.py
+++ ejemplos/ejemplo13.py
@@ -15,17 +15,19 @@ http://www.learnaboutstructures.com/Integration-of-the-Curvature-Diagram-to-find
 from __future__ import print_function
 from dolfin import *
 
-L = 25.
-H = 1.
+#En Sistema Internacional (SI)
+L = 2.5 #en metros en SI
+H = 0.1 #en metros en SI
 Nx = 50
 Ny = 10
 
 mesh = RectangleMesh(Point(0., 0.), Point(L, H), Nx, Ny, "crossed")#Malla estructurada
 
 def eps(v):
-    return sym(grad(v))
+    #return sym(grad(v))
+    return 0.5*(nabla_grad(v) + nabla_grad(v).T)
 
-E = Constant(1e5)
+E = Constant(200.e9) # en N/m2 (acero)
 nu = Constant(0.3)
 
 
@@ -36,9 +38,8 @@ lmbda = 2*mu*lmbda/(lmbda+2*mu)
 def sigma(v):
     return lmbda*tr(eps(v))*Identity(2) + 2.0*mu*eps(v)
 
-
 #Formulación variacional
-rho_g = 1e-3
+rho_g = 7850.0*9.8 # Por ejemplo: acero. Densidad y aceleracion de la gravedad (SI)
 f = Constant((0,-rho_g))
 
 V = VectorFunctionSpace(mesh, 'Lagrange', degree=2)
@@ -57,19 +58,18 @@ bc = DirichletBC(V, Constant((0.,0.)), left)
 u = Function(V, name="Desplazamiento")
 solve(a == l, u, bc)
 
-plot(1e3*u, mode="Desplazamiento")
+plot(u, mode="Desplazamiento")
 
 # Validación
 # ------------------------------
 #
 # Solution dada por teoría de Euler-Bernoulli.
-print("Teoría:", float(3*rho_g*L**4/2/E/H**3))
+print("Teoría:", float((3*rho_g*L**5)/(2*E*(H**2))))
 #Con FEniCS
-print("Deflexión máxima:", -u(L,H/2.)[1])
+print("Deflexión máxima:", -u(L,H/2.0)[1])
 
 
 #Posprocesamiento:
-
 Vsig = TensorFunctionSpace(mesh, "DG", degree=0)
 sig = Function(Vsig, name="Esfuerzo")
 sig.assign(project(sigma(u), Vsig))
@@ -80,22 +80,34 @@ print("Esfuerzo en (0,H):", sig(0, H))
 import matplotlib.pyplot as plt
 
 plt.figure(1)
-plot(1e3*u, mode="displacement")#
+plot(1e2*u, mode="displacement")#
 
 plt.figure(2)
 plot(mesh)
 
 
-plt.show()
+s = sigma(u) - (1./3)*tr(sigma(u))*Identity(2)
+von_Mises = sqrt(3./2*inner(s, s))
+V = FunctionSpace(mesh, 'P', 1)
+von_Mises = project(von_Mises, V)
 
-xdmffile_u = XDMFFile('datos/desplazamiento.xdmf')
-xdmffile_u.write(u,0)
+plt.figure(3)
+plot(von_Mises, title='Tensión intensidad')
+
+
+plt.show()
+#vtkfile_u = File('datos/desplazamiento.pvd')
+#vtkfile_u << u
+
+
+#xdmffile_u = XDMFFile('datos/desplazamiento.xdmf')
+#xdmffile_u.write(u,0)
 
 ## Fields can be exported in a suitable format for vizualisation using Paraview.
 ## VTK-based extensions (.pvd,.vtu) are not suited for multiple fields and parallel
 ## writing/reading. Prefered output format is now .xdmf::
 
-#file_results = XDMFFile("resultados_elasticidad.xdmf")
+#file_results = XDMFFile("datos/resultados_elasticidad.xdmf")
 #file_results.parameters["flush_output"] = True
 #file_results.parameters["functions_share_mesh"] = True
 #file_results.write(u, 0.)
